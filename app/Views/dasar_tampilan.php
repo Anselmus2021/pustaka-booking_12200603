62a0c8c029165f58d37ab3d80918d2f2cd940db1
ansselmuss <anselanselmus12@gmail.com>
2021-10-06 10:35:00 +0700

    Tugas Pertemuan 3

--- app/Views/dasar_tampilan.php
+++ app/Views/dasar_tampilan.php
@@ -1,10 +1,10 @@
 <html>
     <head>
         <title></title>
-        <link href="<?=base_url('/assets/css/booststrap.min.css')?>" rel="stylesheet" />
+        <link href="<?=base_url('/assets/css/bootstrap.min.css')?>" rel="stylesheet" />
     </head>
     <body>
-
+        
         <?=$this->renderSection('isiweb')?>
 
         <script src="<?=base_url('/assets/js/bootstrap.bundle.min.js')?>"></script>
